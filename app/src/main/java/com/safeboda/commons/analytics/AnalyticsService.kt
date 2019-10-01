86b9cc7866e0e0f1e07aced4d144d0058eb0ad2a
Rubén Viciana <ruben@safeboda.com>
2019-10-01 14:32:24 +0200

    Requested changes

--- app/src/main/java/com/safeboda/commons/analytics/AnalyticsService.kt
+++ app/src/main/java/com/safeboda/commons/analytics/AnalyticsService.kt
@@ -1,35 +1,31 @@
 package com.safeboda.commons.analytics
 
-import androidx.annotation.VisibleForTesting
 import com.safeboda.commons.analytics.entity.AnalyticsEvent
 import com.safeboda.commons.analytics.entity.AnalyticsUser
 import com.safeboda.commons.analytics.provider.AnalyticsProvider
 
 class AnalyticsService(
-    analyticsProviders: List<AnalyticsProvider>
+    private val analyticsProviders: List<AnalyticsProvider>
 ) : AnalyticsProvider {
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-    val providerList: List<AnalyticsProvider> = analyticsProviders
-
     override fun setUser(user: AnalyticsUser) {
-        providerList.forEach { provider -> provider.setUser(user) }
+        analyticsProviders.forEach { provider -> provider.setUser(user) }
     }
 
     override fun clearUser() {
-        providerList.forEach { provider -> provider.clearUser() }
+        analyticsProviders.forEach { provider -> provider.clearUser() }
     }
 
     override fun setUserLogged() {
-        providerList.forEach { provider -> provider.setUserLogged() }
+        analyticsProviders.forEach { provider -> provider.setUserLogged() }
     }
 
     override fun setUserNotLogged() {
-        providerList.forEach { provider -> provider.setUserNotLogged() }
+        analyticsProviders.forEach { provider -> provider.setUserNotLogged() }
     }
 
     override fun track(event: AnalyticsEvent) {
-        providerList.forEach { provider -> provider.track(event) }
+        analyticsProviders.forEach { provider -> provider.track(event) }
     }
 
 }
