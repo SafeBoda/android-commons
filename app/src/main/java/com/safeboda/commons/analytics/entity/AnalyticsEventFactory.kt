e7eef8bfa22bb6252e6228e0a3f21063f110f40c
Manuel Perera <mapm14@gmail.com>
2019-11-07 12:11:30 +0100

    Make AnalyticsEventFactory a data class

--- app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsEventFactory.kt
+++ app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsEventFactory.kt
@@ -1,25 +1,23 @@
 package com.safeboda.commons.analytics.entity
 
-class AnalyticsEventFactory(override val name: String) : AnalyticsEvent(name) {
+data class AnalyticsEventFactory(override val name: String) : AnalyticsEvent(name) {
 
     companion object {
         fun <T : Any?> createAnalyticsEvent(
             name: String,
             properties: List<Pair<String, T>> = listOf()
-        ) =
-            AnalyticsEventFactory(name).apply {
-                properties.forEach {
-                    addProperty(it.first, it.second)
-                }
+        ) = AnalyticsEventFactory(name).apply {
+            properties.forEach {
+                addProperty(it.first, it.second)
             }
+        }
 
         fun <T : Any?> createAnalyticsEventWithListValues(
             name: String,
             pair: Pair<String, List<T>>
-        ) =
-            AnalyticsEventFactory(name).apply {
-                addListOfProperties(pair.first, pair.second)
-            }
+        ) = AnalyticsEventFactory(name).apply {
+            addListOfProperties(pair.first, pair.second)
+        }
     }
 
 }
