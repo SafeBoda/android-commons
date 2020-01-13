e813d9d12e6bf0b0940f68bed5974c42e315886e
Iosu <iosu@safeboda.com>
2020-01-13 10:06:04 +0100

    [FEAT] add @Size(max = 40) to event name

--- app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsEventFactory.kt
+++ app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsEventFactory.kt
@@ -1,10 +1,12 @@
 package com.safeboda.commons.analytics.entity
 
+import androidx.annotation.Size
+
 data class AnalyticsEventFactory(override val name: String) : AnalyticsEvent(name) {
 
     companion object {
         fun <T : Any?> createAnalyticsEvent(
-            name: String,
+            @Size(max = 40) name: String,
             properties: List<Pair<String, T>> = listOf()
         ) = AnalyticsEventFactory(name).apply {
             properties.forEach {
@@ -13,7 +15,7 @@ data class AnalyticsEventFactory(override val name: String) : AnalyticsEvent(nam
         }
 
         fun <T : Any?> createAnalyticsEventWithListValues(
-            name: String,
+            @Size(max = 40) name: String,
             pair: Pair<String, List<T>>
         ) = AnalyticsEventFactory(name).apply {
             addListOfProperties(pair.first, pair.second)
