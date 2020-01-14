8149efd0932def822b34d3adf6f4d262cbd18d42
Rubén Viciana <ruben@safeboda.com>
2020-01-14 12:01:59 +0100

    Add CleverTap analytics client

---
+++ app/src/main/java/com/safeboda/commons/analytics/provider/CleverTapAnalyticsProvider.kt
@@ -0,0 +1,51 @@
+package com.safeboda.commons.analytics.provider
+
+import android.app.Activity
+import android.content.Context
+import com.clevertap.android.sdk.CleverTapAPI
+import com.safeboda.commons.analytics.entity.AnalyticsEvent
+import com.safeboda.commons.analytics.entity.AnalyticsEventFactory
+import com.safeboda.commons.analytics.entity.AnalyticsUser
+import com.safeboda.commons.analytics.entity.IS_USER_LOGGED_IN
+
+class CleverTapAnalyticsProvider(
+    context: Context
+) : AnalyticsProvider {
+
+    private val cleverTap = CleverTapAPI.getDefaultInstance(context)
+
+    override fun setUser(user: AnalyticsUser) {
+        cleverTap?.pushProfile(user.getProperties())
+    }
+
+    override fun clearUser(user: AnalyticsUser) {
+        cleverTap?.removeValueForKey(user.id.toString())
+    }
+
+    override fun setUserLogged() {
+        setUserSessionStatus(true)
+    }
+
+    override fun setUserNotLogged() {
+        setUserSessionStatus(false)
+    }
+
+    override fun track(event: AnalyticsEvent) {
+        cleverTap?.pushEvent(event.name)
+    }
+
+    override fun trackScreen(activity: Activity?, screenName: String, overrideScreenClass: String?) {
+        activity?.let {
+            val event: AnalyticsEvent = AnalyticsEventFactory.createAnalyticsEvent<AnalyticsEvent>(name = screenName)
+            track(event)
+        }
+    }
+
+    private fun setUserSessionStatus(isLoggedIn: Boolean) {
+        val profileUpdate = HashMap<String, Any>().apply {
+            put(IS_USER_LOGGED_IN, isLoggedIn)
+        }
+        cleverTap?.pushProfile(profileUpdate)
+    }
+
+}
