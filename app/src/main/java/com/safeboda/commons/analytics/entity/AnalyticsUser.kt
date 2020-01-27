f2db5edd8c3c0e44efbfc5890e4e99a311721c25
Rubén Viciana <ruben@safeboda.com>
2020-01-27 15:48:16 +0300

    Add new properties to the AnalyticsUser

--- app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsUser.kt
+++ app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsUser.kt
@@ -2,12 +2,18 @@ package com.safeboda.commons.analytics.entity
 
 abstract class AnalyticsUser(
     val id: Long?,
-    val identifier: String?
+    val identifier: String?,
+    val firstName: String?,
+    val email: String?
 ) {
 
     private val properties: MutableMap<String, Any?> = mutableMapOf(
         USER_ID to id,
-        USER_IDENTIFIER to identifier
+        USER_IDENTIFIER to identifier,
+        USER_NAME to firstName,
+        USER_IDENTITY to id,
+        USER_EMAIL to email,
+        USER_PHONE to identifier
     )
 
     fun getProperties(): Map<String, Any?> = properties
