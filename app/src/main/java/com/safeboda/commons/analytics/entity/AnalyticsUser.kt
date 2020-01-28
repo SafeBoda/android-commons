13c71dd4a30c7d3d0ad64cc4cb3e3b3a3af9178f
Rubén Viciana <ruben@safeboda.com>
2020-01-28 14:19:23 +0300

    Add open values in order to override them

--- app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsUser.kt
+++ app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsUser.kt
@@ -3,8 +3,8 @@ package com.safeboda.commons.analytics.entity
 abstract class AnalyticsUser(
     val id: Long?,
     val identifier: String?,
-    val firstName: String?,
-    val email: String?
+    open val firstName: String?,
+    open val email: String?
 ) {
 
     private val properties: MutableMap<String, Any?> = mutableMapOf(
