35b0dc7c26dfc4fe198a9c72def49437f4de1fa9
Youssef Shamass <89531205+yshamass@users.noreply.github.com>
2022-04-19 13:05:23 +0300

    track user location in AnalyticsUser.kt and update profile properties map accordingly (#50)

--- app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsUser.kt
+++ app/src/main/java/com/safeboda/commons/analytics/entity/AnalyticsUser.kt
@@ -4,13 +4,20 @@ abstract class AnalyticsUser(
     val id: String?,
     val identifier: String?,
     val name: String?,
-    open val email: String?
+    open val email: String?,
+    location: String? = null
 ) {
 
     private val properties: MutableMap<String, Any?> = mutableMapOf(
         USER_ID to id,
         USER_IDENTIFIER to identifier
     )
+
+    init {
+        location?.let {
+            addProperty(USER_LOCATION, it)
+        }
+    }
 
     fun getProperties(): Map<String, Any?> = properties
 
